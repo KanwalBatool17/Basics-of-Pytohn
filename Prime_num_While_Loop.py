a0ffe2c3a6c22157fb2aa1a675e6b4dfb94f9180
KanwalBatool17 <kanwalbatool579@gmail.com>
2020-09-12 23:21:59 +0500

    1 - Finding Primr number using While loop.

---
+++ Prime_num_While_Loop.py
@@ -0,0 +1,13 @@
+print("\n____________________* Prime Number with While Loop *___________________\n")
+Number = 1
+while (Number <= 100):
+    count = 0
+    i = 2
+    while (i <= Number// 2):
+        if (Number % i == 0):
+            count = count + 1
+            break
+        i = i + 1
+    if (count == 0 and Number != 1):
+        print( Number)
+    Number = Number + 1
