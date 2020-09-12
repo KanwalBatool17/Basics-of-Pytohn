8a38f8a4daf683f1cb5aec609d4c6219be7f0d9d
KanwalBatool17 <kanwalbatool579@gmail.com>
2020-09-12 22:53:58 +0500

    1 - Finding odd number using while loop. 2 - finding even number using for loop.

---
+++ Even_Odd_num_While_Loop.py
@@ -0,0 +1,8 @@
+print("\n____________________* Even and Odd Number with While Loop *___________________\n")
+num = 1
+while num <= 100:
+    if num % 2 == 0:
+        print(num , "is even number")
+    elif num % 2 == 1:
+        print(num, "is odd number")
+    num = num + 1
